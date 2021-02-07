cb2d66f40cf104292de85689f4c2da8deafb722c
ade-omi <omiyale@sheridancollege.ca>
2021-02-07 13:39:12 -0500

    First Commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentlist;
+
+import java.util.Scanner;
+
+/**
+ *This class is a simple example of creating arrays of objects
+ * 
+ * @author dapo
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Array of students
+        Student[] students = new Student[2];
+        
+        Scanner input = new Scanner(System.in);
+        //Loop to create studnets
+        
+        for(int i =0; i <students.length; i++){
+            System.out.println("What are the students names");
+            String name = input.nextLine();
+            Student student = new Student(name);
+            
+            students[i] = student;
+        }
+        System.out.println("Printing the students: ");
+        String format = "The student's name is %s\n";
+        for(Student student: students){
+            System.out.printf(format, student.getName());
+        }
+        
+    }
+    
+}
